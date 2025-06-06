46a3fb84790a9aaa92fe9da83cae537a9dab3d2c
Yazalde Amaya <yazalde.amaya@gmail.com>
2025-06-06 12:54:36 -0400

    AddRemoveTotal

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,28 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        valorCosto=parseFloat(item.cost.substring(1));
+        total += valorCosto * item.quantity;
+    });    
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    }else{
+        dispatch(removeItem(item));
+    }   
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    costoItem = parseFloat(item.cost.substring(1));
+    totalCostItem = costoItem * item.quantity;
   };
+
+  const handleCheckoutShopping = (e) => {
+   alert('Functionality to be added for future reference');
+  };  
 
   return (
     <div className="cart-container">
