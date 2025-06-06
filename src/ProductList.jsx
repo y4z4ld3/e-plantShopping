a8bc9bc5942d939ffbbe8091cf16c52eb6243dcb
Yazalde Amaya <yazalde.amaya@gmail.com>
2025-06-06 14:57:26 -0400

    AjustesFinales

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -261,7 +261,10 @@ function ProductList({ onHomeClick }) {
             ...prevState, // Spread the previous state to retain existing entries
             [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
-    };    
+    };
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };        
     return (
         <div>
             <div className="navbar" style={styleObj}>
